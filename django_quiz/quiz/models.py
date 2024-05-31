45201c84a828991233ecd60f45bf3a25496e6986
ana-lam <anakristinalam@gmail.com>
2024-05-31 14:06:35 -0400

    quiz taker test

--- django_quiz/quiz/models.py
+++ django_quiz/quiz/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 
 class Quiz(models.Model):
+
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=200)
 
     def __str__(self):
         return self.title
 
 class Question(models.Model):
+
+    # id = models.AutoField(primary_key=True)
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     question_text = models.CharField(max_length=500, default="Default question text")  # Add default value here
     correct_answer = models.CharField(max_length=200)
@@ -24,3 +28,38 @@ class Answer(models.Model):
   text = models.CharField(max_length=300)
   is_correct = models.BooleanField(default=False)
 
+# to collect quiz taker data
+class QuizTaker(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=500)
+    last_name = models.CharField(max_length=500)
+
+    NO_CODING = 'NOCD'
+    BEGINNER = 'BGNR'
+    INTERMEDIATE = 'INTR'
+    ADVANCED = 'ADVD'
+
+    self_reported_ability_choices = [
+        (NO_CODING, 'No Coding Experience'),
+        (BEGINNER, 'Beginner'),
+        (INTERMEDIATE, 'Intermediate'),
+        (ADVANCED, 'Advanced')
+    ]
+
+    programming_ability = models.CharField(max_length=4, choices=self_reported_ability_choices, default=BEGINNER)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
+
+# if we want to give questions tags
+class CategoryTag(models.Model):
+
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    tags = models.ManyToManyField(Question)
+
+    def __str__(self):
+        return self.name
+# bare bones example
