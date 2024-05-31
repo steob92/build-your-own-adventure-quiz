0e8aeb56624d850336bf1304421e2d09215f21e7
ana-lam <anakristinalam@gmail.com>
2024-05-31 16:20:12 -0400

    add admin

--- django_dir/sim/admin.py
+++ django_dir/sim/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
+from .models import Quiz, Question, Answer, QuizTaker, CategoryTag
+
 # Register your models here.
+admin.site.register(Quiz)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(QuizTaker)
+admin.site.register(CategoryTag)
