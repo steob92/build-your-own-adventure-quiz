aec1b45b62e604de030911f3d2a7b5c651d10281
ana-lam <anakristinalam@gmail.com>
2024-05-31 14:08:44 -0400

    delete quiz

--- django_quiz/quiz_project/quiz/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.quiz_list, name='quiz_list'),
-    path('<int:quiz_id>/', views.take_quiz, name='take_quiz'),
-    path('create_quiztaker/', views.create_quiztaker, name='create_quiztaker'),
-]
