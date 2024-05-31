dc87c1d864db40fe23258fffc9efc0288a2d0573
Yasmeen Asali <yasmeenasali@gmail.com>
2024-05-31 14:22:16 -0400

    some version thats not working

--- django_quiz/quiz/models.py
+++ django_quiz/quiz/models.py
@@ -7,17 +7,22 @@ class Quiz(models.Model):
         return self.title
 
 class Question(models.Model):
+    QUESTION_TYPES = (
+        ('MC', 'Multiple Choice'),
+        ('OE', 'Open Ended'),
+    )
+
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
-    question_text = models.CharField(max_length=500, default="Default question text")  # Add default value here
+    question_text = models.CharField(max_length=500)
+    question_type = models.CharField(max_length=2, choices=QUESTION_TYPES, default='MC')
     correct_answer = models.CharField(max_length=200)
-    option1 = models.CharField(max_length=200)
-    option2 = models.CharField(max_length=200)
-    option3 = models.CharField(max_length=200)
-    option4 = models.CharField(max_length=200)
+    option1 = models.CharField(max_length=200, blank=True, null=True)
+    option2 = models.CharField(max_length=200, blank=True, null=True)
+    option3 = models.CharField(max_length=200, blank=True, null=True)
+    option4 = models.CharField(max_length=200, blank=True, null=True)
 
     def __str__(self):
         return self.question_text
-
 
 class Answer(models.Model):
   question = models.ForeignKey(Question, on_delete=models.CASCADE)
